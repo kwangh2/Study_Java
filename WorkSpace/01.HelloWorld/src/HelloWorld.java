7c6478f44b1f55e22846a87c12bf4b54f2882402
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2023-09-11 17:46:16 +0900

    자바 첫날 수업 완료
    main method 에서 작업
    syso - 콘솔창에 출력

---
+++ WorkSpace/01.HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+// <- 주석, 기계가 컴파일 하지않는 코드, 인간이 보기위한 설명
+// 1. main : 메인 메소드라고하며 우리가 당분간 계속해서 사용할 프로그램의 시작점.
+// 2. syso : 프로그램이 진행 되는 중에 어떤 문자나 결과를 출력하기 위한 명령어.
+// 3. ctrl + space : 코드의 자동 완성
+// 4. ctrl + s / ctrl + shift + s : 현자 파일 저장 , 전체 파일 저장
+
+// Q. 내 이니셜 출력
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloworldJKH");
+	}
+
+}
---
+++ WorkSpace/01.HelloWorld/src/JKH.java
@@ -0,0 +1,6 @@
+
+public class JKH {
+	public static void main(String[] args) {
+		System.out.println("JKH");
+	}
+}
