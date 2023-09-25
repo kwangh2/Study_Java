a287485ba155d357b95ca0f467501ed22c18bb3f
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2023-09-25 17:47:03 +0900

    Create Text02_Array.java

---
+++ WorkSpace/05.Reference/src/pack02/exarray1/Text02_Array.java
@@ -0,0 +1,14 @@
+package pack02.exarray1;
+
+public class Text02_Array {
+	public static void main(String[] args) {
+		int[] intArr = new int[9];
+		
+		//2 x i = 2*i
+		for (int i=0 ; i<intArr.length; i++) {
+			intArr[i] = i+1;
+			System.out.println("2 x " + (i) + "=" + (2*i));
+			
+		}
+	}
+}
