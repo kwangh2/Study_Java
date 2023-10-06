e0e6e32503883d2ba715c88980b880629aaf89b6
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2023-10-06 17:42:40 +0900

    Update BJ3003_chess.java

--- WorkSpace/Baekjoon/src/BJ3003_chess.java
+++ WorkSpace/Baekjoon/src/BJ3003_chess.java
@@ -1,10 +1,16 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BJ3003_chess {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] ChessF = { 1, 1, 2, 2, 2, 8 };
-		int[] ChessD = new int[ChessF.length];
-//		for (int i )
+		int[] chessF = { 1, 1, 2, 2, 2, 8 }; // 체스말
+		int[] chessD = new int[chessF.length];// 동수말
+		int[] chessGap = new int[chessF.length];// 동수말
+		for (int i = 0 ; i < chessF.length; i++) {
+			chessD[i] = sc.nextInt();
+			chessGap[i] =  chessF[i] - chessD[i];
+		}// 동수 말 입력
+		System.out.println(Arrays.toString(chessGap));
 	}
 }
